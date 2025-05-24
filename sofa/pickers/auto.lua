308acaa093e302f825ec886f918fe4041cdc550d
dadez <dadez@pm.me>
2025-05-24 11:13:09 +0200

    chore(pickers): set default to fuzzel

--- sofa/pickers/auto.lua
+++ sofa/pickers/auto.lua
@@ -3,5 +3,5 @@ local os = require("os")
 if os.getenv("DISPLAY") == nil then
   return require("sofa.pickers.fzf")
 else
-  return require("sofa.pickers.rofi")
+  return require("sofa.pickers.fuzzel")
 end
